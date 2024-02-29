3bbdfbded5372feaaf27a69d3747698b5cff418a
GianFTZ <gianfelipetz@hotmail.com>
2024-02-29 14:46:13 -0300

    feat: create api1 provider contract

---
+++ src/domain/contracts/api-1-provider.ts
@@ -0,0 +1,6 @@
+type Api1Response = {
+  id: string;
+  name: string;
+};
+
+export type Api1Provider = (props: { name: string }) => Promise<Api1Response>
--- src/domain/features/multiple-api-data-process.ts
+++ src/domain/features/multiple-api-data-process.ts
@@ -1,3 +1,5 @@
+import type { Api1Provider } from "../contracts/api-1-provider"
+
 type Input = {
   id: string
   name: string
@@ -5,7 +7,9 @@ type Input = {
 }
 type Output = {}
 type MultipleApiDataProcess = (input: Input) => Promise<Output>
-type SetupMultipleApiDataProcessProps = {}
+type SetupMultipleApiDataProcessProps = {
+  api1Provider: Api1Provider
+}
 type SetupMultipleApiDataProcess = (props: SetupMultipleApiDataProcessProps) => MultipleApiDataProcess
 
 export const setupMultipleApiDataProcess: SetupMultipleApiDataProcess = ({}: SetupMultipleApiDataProcessProps) => async input => {
