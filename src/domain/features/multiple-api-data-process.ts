9a5a9e7c2a59938e11a8c388250feb49eaaeb94f
GianFTZ <gianfelipetz@hotmail.com>
2024-02-29 14:43:45 -0300

    feat: typed I/O at MultipleApiDataProcess service

--- src/domain/features/multiple-api-data-process.ts
+++ src/domain/features/multiple-api-data-process.ts
@@ -1,9 +1,13 @@
-type Input = {}
+type Input = {
+  id: string
+  name: string
+  birthDate: string
+}
 type Output = {}
 type MultipleApiDataProcess = (input: Input) => Promise<Output>
 type SetupMultipleApiDataProcessProps = {}
 type SetupMultipleApiDataProcess = (props: SetupMultipleApiDataProcessProps) => MultipleApiDataProcess
 
-export const setupMultipleApiDataProcess: SetupMultipleApiDataProcess = () => async input => {
+export const setupMultipleApiDataProcess: SetupMultipleApiDataProcess = ({}: SetupMultipleApiDataProcessProps) => async input => {
   return {}
 }
