88c7d4b2e0b6ac3b58e500de8e213afb1f25638d
GianFTZ <gianfelipetz@hotmail.com>
2024-02-29 17:33:38 -0300

    feat: implements dangerous retry mechanism

--- src/domain/features/multiple-api-data-process.ts
+++ src/domain/features/multiple-api-data-process.ts
@@ -1,8 +1,6 @@
 import type { AlertaObservabilidadeProvider, Api1Provider, Api2Provider, Api3Provider, LocalCalcProvider } from "../contracts"
 import { CorruptedDataError, InternalError, NotFoundError, TimeoutError } from "../errors"
 import { Errors, type ErrorHandler } from "../../utils"
-import { pino } from "../../.."
-
 type Input = {
   name: string
 }
@@ -19,27 +17,36 @@ type SetupMultipleApiDataProcessProps = {
 type SetupMultipleApiDataProcess = (props: SetupMultipleApiDataProcessProps) => MultipleApiDataProcess
 
 export const setupMultipleApiDataProcess: SetupMultipleApiDataProcess = ({ api1Provider, api2Provider, localCalc, api3Provider, alertaObservabilidadeProvider, errorHandler }: SetupMultipleApiDataProcessProps) => async input => {
-  try {
-    const { id, name } = await api1Provider({ name: input.name })
-    errorHandler({ code: id, cause: name, from: "Api1Provider"})
-    const { dataNascimento } = await api2Provider({ id })
-    const success = await localCalc({ name, dataNascimento, id })
-    await api3Provider({ id, isBirthDay: success })
-    return "Success"
-  } catch (e) {
-    console.log(JSON.stringify(e))
-    if(e instanceof NotFoundError) {
-      return "NotFound"
-    } else if(e instanceof CorruptedDataError) {
-      alertaObservabilidadeProvider({ id: Errors.Corrupted })
-      return "CorruptedData"
-    } else if(e instanceof InternalError) {
-      alertaObservabilidadeProvider({ id: Errors.Internal })
-      return "InternalError"
-    } else if(e instanceof TimeoutError) {
-      return "TimedOut"
-    } else {
-      return "Unknown"
+  let attempts = 0
+  async function DangerousRetryMechanism() {
+    try {
+      const { id, name } = await api1Provider({ name: input.name })
+      errorHandler({ code: id, cause: name, from: "Api1Provider"})
+      const { dataNascimento } = await api2Provider({ id })
+      const success = await localCalc({ name, dataNascimento, id })
+      await api3Provider({ id, isBirthDay: success })
+      return "Success"
+    } catch (e) {
+      console.log(JSON.stringify(e))
+      if(e instanceof NotFoundError) {
+        return "NotFound"
+      } else if(e instanceof CorruptedDataError) {
+        alertaObservabilidadeProvider({ id: Errors.Corrupted })
+        return "CorruptedData"
+      } else if(e instanceof InternalError) {
+        alertaObservabilidadeProvider({ id: Errors.Internal })
+        return "InternalError"
+      } else if(e instanceof TimeoutError) {
+        if(attempts != 0) {
+          attempts++ 
+          return DangerousRetryMechanism()
+        }
+        return "TimedOut"
+      } else {
+        return "Unknown"
+      }
     }
   }
+  return await DangerousRetryMechanism()
+  
 }
